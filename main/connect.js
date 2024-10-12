3cee621291f83a7399fc135ebfd4f2eb98fcfcea
Htet Ko Ko <htetkokoemail@gmail.com>
2024-10-12 16:21:43 +0630

    Update connect.js

--- main/connect.js
+++ main/connect.js
@@ -1,3 +1,4 @@
+const blog = angular.module('blog', []);
 const token = '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';
 const repo = 'htetkokoblog/htetkoko-blog'; // သင့်ရဲ့ repository
 const path = 'contents/postdata.json'; // JSON ဖိုင်ရဲ့ လမ်းကြောင်း
