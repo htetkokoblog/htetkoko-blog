e8879c407a1ec8de7b3cb7bc7521c2bf67f302bb
Htet Ko Ko <htetkokoemail@gmail.com>
2024-10-12 10:06:27 +0630

    Create connect.js

---
+++ main/connect.js
@@ -0,0 +1,4 @@
+const token = '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';
+const repo = 'htetkokoblog/htetkoko-blog'; // သင့်ရဲ့ repository
+const path = 'contents/postdata.json'; // JSON ဖိုင်ရဲ့ လမ်းကြောင်း
+const url = `https://api.github.com/repos/${repo}/contents/${path}`;
