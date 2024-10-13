ad5f44779594e5f717457ec94db5f815f485f0a7
Htet Ko Ko <htetkokoemail@gmail.com>
2024-10-13 22:55:41 +0630

    Update request.js

--- main/request.js
+++ main/request.js
@@ -1,28 +1,14 @@
 //Request Get Data
-
+const blog = angular.module('blog', []);
 blog.controller('myblog', function($scope, $http) {
-    async function getJsonFile() {
-        try {
-            const response = await fetch(url, {
-                headers: {
-                    'Authorization': `token ${token}`,
-                    'Accept': 'application/vnd.github.v3+json'
-                }
+    function getJsonFile() {
+        $http.get('https://htetkoko-blog.vercel.app/main/postdata.json')
+            .then(function(response) {
+                $scope.data = response.data;
+                $scope.data.reverse(); // Reverse the array if needed
+            }, function(error) {
+                console.error('Error fetching data:', error);
             });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-
-            const fileData = await response.json();
-            const content = atob(fileData.content); // base64_decode
-            const decoded = decodeURIComponent(escape(content));
-            $scope.data = JSON.parse(decoded);// Set data to scope
-            $scope.data.reverse(); // Reverse the array if needed
-            $scope.$apply(); // Notify AngularJS about the changes
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
     }
     getJsonFile();
 });
