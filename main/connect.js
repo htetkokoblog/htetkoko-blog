212c95583d56e0c4e0f029430d49a776d90cc42b
Htet Ko Ko <htetkokoemail@gmail.com>
2024-10-13 22:56:07 +0630

    Delete main/connect.js

--- main/connect.js
+++
@@ -1,5 +0,0 @@
-const blog = angular.module('blog', []);
-const token = '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';
-const repo = 'htetkokoblog/My-App'; // သင့်ရဲ့ repository
-const path = 'api/htetkoko-blog/postdata.json'; // JSON ဖိုင်ရဲ့ လမ်းကြောင်း
-const url = `https://api.github.com/repos/${repo}/contents/${path}`;
