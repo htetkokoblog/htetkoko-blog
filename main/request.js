3e12b584be179ac4772f51a70ac74fecd17b2e29
Htet Ko Ko <htetkokoemail@gmail.com>
2024-10-16 00:49:58 +0630

    Update request.js

--- main/request.js
+++ main/request.js
@@ -14,7 +14,7 @@ blog.controller('myblog', function($scope, $http, $location, $sce) {
     });
 
     function getJsonFile() {
-        $http.get('/api/data')
+        $http.get('https://htetkokoblog.vercel.app/api/data')
             .then(function(response) {
                 $scope.data = response.data;
                 $scope.data.reverse(); // Reverse the array if needed
